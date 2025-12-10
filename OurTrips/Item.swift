deff92724b2b8f1fab7100dccc3ca325c077c087
Avinash Dimmeta <avinash514@gmail.com>
2025-12-09 22:55:28 -0600

    Initial Commit

---
+++ OurTrips/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  OurTrips
+//
+//  Created by Avinash Dimmeta on 12/9/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ OurTripsTests/OurTripsTests.swift
@@ -0,0 +1,16 @@
+//
+//  OurTripsTests.swift
+//  OurTripsTests
+//
+//  Created by Avinash Dimmeta on 12/9/25.
+//
+
+import Testing
+
+struct OurTripsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
